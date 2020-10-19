a672dea82a24cb4b5d10c6d299aec457fd447b69
T. Tran <t-tran@users.noreply.github.com>
2020-10-19 17:21:52 +0700

    Remove unmodified files

--- build.sh
+++ build.sh
@@ -2,14 +2,13 @@
 
 function build_in_docker() {
   v_build="manjaro_build"
-  v_pacman="manjaro_pacman"
   docker volume create $v_build
   docker volume create $v_pacman
   docker run --rm --privileged --cap-add=SYS_ADMIN -it \
          --mount type=bind,src=$(pwd),dst=/vigo/ \
          --mount type=volume,src=$v_build,dst=/var/lib/manjaro-tools/ \
-         --mount type=volume,src=$v_pacman,dst=/var/cache/pacman/ \
-         --mount type=bind,src=$(pwd)/cache/,dst=/var/cache/manjaro-tools/ \
+         --mount type=bind,src=$(pwd)/cache/pacman/,dst=/var/cache/pacman/ \
+         --mount type=bind,src=$(pwd)/cache/manjaro-tools/,dst=/var/cache/manjaro-tools/ \
          --workdir /vigo/ \
          -e IN_DOCKER=true \
          busyrack/vigo:dev \
