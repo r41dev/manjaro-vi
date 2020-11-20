b64a11d1e4f6cdf51706475da70f861c45c87ed1
SoapIsGreen <green.soap16@gmail.com>
2020-11-20 09:25:52 +0700

    fix rm.sh remove autostart-script which opening Vigo Store service

--- iso-profiles/manjaro/kde/desktop-overlay/usr/share/vigo/rm.sh
+++ iso-profiles/manjaro/kde/desktop-overlay/usr/share/vigo/rm.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-isStartVigo=0
-if [[ -f /tmp/tmp1 ]]; then
-  isStartVigo=1
-  exit
-fi
-if [[ $isStartVigo == 1 ]]; then
-#bash /usr/share/vigo/open-in-ie/install.sh
-rm ~/.config/autostart/org.kde.yakuake.desktop ~/.config/autostart/vigo-store-app.desktop ~/.config/autostart/rm.desktop
-
+if [[ -e /tmp/tmp1 ]]; then
+  rm ~/.config/autostart/org.kde.yakuake.desktop ~/.config/autostart/vigo-store-app.desktop ~/.config/autostart/rm.desktop
 fi
